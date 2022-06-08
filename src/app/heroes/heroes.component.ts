4a009178f04b9a33eea62bdcc44680126388b200
Brendan Safr <brendansafr@gmail.com>
2022-06-07 21:52:43 -0400

    Displaying heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HEROES } from '../mock-heroes';
+
 import { Hero } from '../hero';
 
 @Component({
@@ -8,10 +10,7 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
 
   constructor() {}
 
